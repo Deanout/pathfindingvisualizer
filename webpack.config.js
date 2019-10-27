8756a835a69e6bede5e3db940a814332785313ea
Deanout <dean@deanout.com>
2019-10-27 10:45:30 -0500

    Fixed issue with webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
+const path = require("path");
 module.exports = {
+  context: path.join(__dirname, "/"),
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js"
+  },
   module: {
     rules: [
       {
