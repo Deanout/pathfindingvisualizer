aca55f41be3aeacb67267f44b5de95a3cdeeccfc
Deanout <dean@deanout.com>
2019-10-27 11:09:39 -0500

    Change webpack config again.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
-const path = require("path");
 module.exports = {
-  context: path.join(__dirname, "/"),
+  entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "build"),
-    filename: "bundle.js"
+    filename: "bundle.[hash].js"
   },
   module: {
     rules: [
